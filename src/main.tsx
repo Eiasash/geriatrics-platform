153a954d7ec9fab11161bff0e686e8518ee233bd
User <user@example.com>
2025-08-13 00:34:01 +0300

    Add complete project structure for Netlify deployment

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ShaareZedekGeriatricsPlatform from './ShaareZedekGeriatricsPlatform';
+import './index.css'; // We will create this next
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ShaareZedekGeriatricsPlatform />
+  </React.StrictMode>,
+)
